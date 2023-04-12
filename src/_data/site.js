8d45048c1aea84bb2c61ca7cb68b65a9f08a6e34
Jesse Gardner <jesse@plasticmind.com>
2023-04-12 19:25:28 -0400

    Updated site URL

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://practicaldesignsystems.netlify.app',
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://practicaldesignsystems.com',
     title: process.env.ELEVENTY_ENV === 'development' ? '🔴 Practical Design Systems' : 'Practical Design Systems',
     description: "Better collaboration between design and engineering teams."
 };
