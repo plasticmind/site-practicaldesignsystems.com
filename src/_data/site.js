710800a9cae5c6bf35dced9ab09ab890c7945c06
Jesse Gardner <jesse@plasticmind.com>
2023-04-13 05:19:33 -0400

    Added services page

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,7 @@
 module.exports = {
+    buildDate: new Date(),
     url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://practicaldesignsystems.com',
     title: process.env.ELEVENTY_ENV === 'development' ? '🔴 Practical Design Systems' : 'Practical Design Systems',
-    description: "Better collaboration between design and engineering teams."
+    description: "Better collaboration between design and engineering teams.",
+    twitter: "@plasticmind"
 };
